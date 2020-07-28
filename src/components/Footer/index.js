39bc2837dd1753fab1e778810fe8c63b83c59f0b
Igor Frey <igorfreyy97@gmail.com>
2020-07-27 22:19:48 -0300

    retirando imagem do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <FooterBase>
       <a href="">
-        <img src="https://media.discordapp.net/attachments/703328683120787527/722056126023073822/unknown.png" alt="New Power" />
+        <img src="" alt="New Power" />
       </a>
       <p>
         Red Nation
